4d523817194acac2b228e6df61f946bad29b9ea2
Shuvro <118240287+shuvrobean@users.noreply.github.com>
2022-11-15 13:18:13 +0900

    Add files via upload

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.7;
 
 
 contract SimpleStorage{
-    
-    uint256 a;
+    // a is visible because it is put as public
+    uint256 public a;
     function store(uint256 b) public {
         a=b;
+        a=a+1;
     }
-    
+    function retrieve() public view returns(uint256){
+        return a;
+    }
 }
